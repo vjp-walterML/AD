d22f315d4be071fb4b85f842003d2b0283dcfb57
vjp-walterML <wmartinl01@informatica.iesvalledeljerteplasencia.es>
2024-01-08 14:21:02 +0100

    casa

---
+++ UT5-Acceso web con SpringBoot/SpringContactMVC/src/main/java/com/iesvjp/SpringContactMvcApplication.java
@@ -0,0 +1,13 @@
+package com.iesvjp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringContactMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringContactMvcApplication.class, args);
+	}
+
+}
